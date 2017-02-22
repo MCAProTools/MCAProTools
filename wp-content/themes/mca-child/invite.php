974e42e5c0f6a2c99c50ca262422f5dc1e6d2d2d
tarundhanwani <tarundhanwani@gmail.com>
2017-02-23 03:28:21 +0800

    Creation of the invite page template

---
+++ wp-content/themes/mca-child/invite.php
@@ -0,0 +1,119 @@
+<?php
+/* Template Name: invite */
+get_header(); ?>
+
+    <div id="content" class="full-width">
+        <div id="post-1467" class="post-1467 page type-page status-publish hentry">
+            <span class="entry-title" style="display: none;">MCA Tools Sales Page (Upsell)</span><span class="vcard" style="display: none;"><span class="fn"><a href="http://mcaprotools.com/author/ptadmin/" title="Posts by MCAPT Admin" rel="author">MCAPT Admin</a></span></span><span class="updated" style="display:none;">2017-02-17T15:15:34+00:00</span>						<div class="post-content">
+                <div class="container">
+                    <center><p></p>
+                        <h2 class="get-the-1" style="font-size: 60px; margin-bottom: 0px !important;">GET THE #1 MARKETING SYSTEM FOR</h2>
+                        <h1 class="motor-club-text" style="font-size: 80px; margin-top: 0px !important; margin-bottom: 25px;">MOTOR CLUB OF AMERICA!</h1>
+                        <p><img class=" size-full wp-image-591 aligncenter" src="http://mcaprotools.s3-us-west-2.amazonaws.com/wp-content/uploads/2015/03/mcatools.png" alt="mcatools" width="1071" height="564"></p>
+                        <h1 style="font-size: 90px;">WHAT DO YOU GET?</h1>
+                        <h2>Find out why we’re the #1 marketing tool for MCA!</h2>
+                        <p></p></center><p></p>
+                    <div class="container bullet-points-container">
+                        <div class="get_list invite-1">
+                            <div class="col-md-6">
+                                <h2>Personalized Sales Pages</h2>
+                                <p>Our marketing systems consistently<br>
+                                    generate some of the highest optin<br>
+                                    rates in the industry.</p>
+                            </div>
+                            <div class="col-md-6">
+                                <h2>Detailed Presentation Video</h2>
+                                <p>We make sure to included everything your<br>
+                                    prospects need to know about MCA in an<br>
+                                    attractive video presensation…</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="container bullet-points-container">
+                        <div class="get_list invite-2">
+                            <div class="col-md-6">
+                                <h2>Add Your Own Videos</h2>
+                                <p>We make it easy for you to brand yourself<br>
+                                    by changing any of our videos with your<br>
+                                    own, just use the embed code and your<br>
+                                    ready to go!</p>
+                            </div>
+                            <div class="col-md-6">
+                                <h2>Traffic Stats &amp; Analytics</h2>
+                                <p>We all want to know how well our sales funnels<br>
+                                    are doing, so we provide the most important<br>
+                                    information to you so you can make the best<br>
+                                    decisions about your marketing efforts.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="container bullet-points-container">
+                        <div class="get_list invite-3">
+                            <div class="col-md-6">
+                                <h2>1 – 2 – 3 Business Training</h2>
+                                <p>Getting started online is not always the<br>
+                                    easiest if your new, we provide training<br>
+                                    course to guide &amp; help set you up!</p>
+                            </div>
+                            <div class="col-md-6">
+                                <h2>Creative Graphics</h2>
+                                <p>We provide you with graphics to use on your<br>
+                                    social media posts, or your personal websites.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="container bullet-points-container">
+                        <div class="get_list invite-4">
+                            <div class="col-md-6">
+                                <h2>Discounted Print Materials</h2>
+                                <p>We have partnered with some of the top<br>
+                                    print providers in the industry to bring<br>
+                                    you low cost high quality business cards,<br>
+                                    handouts &amp; other print materials!</p>
+                            </div>
+                            <div class="col-md-6">
+                                <h2>Social Media Tools</h2>
+                                <p>We make it easy to share your sales pages &amp;<br>
+                                    customize the “message” of any post right from<br>
+                                    our marketing hub. Compatiable with all major<br>
+                                    social media platforms!</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="container bullet-points-container">
+                        <div class="get_list invite-5">
+                            <div class="col-md-6">
+                                <h2>Autoresponder Series</h2>
+                                <p>You get 20+ high converting professionally<br>
+                                    written email follow up series for MCA &amp; MCA<br>
+                                    Pro Tools!</p>
+                            </div>
+                            <div class="col-md-6">
+                                <h2>Everything You Need</h2>
+                                <p>Thats right! We got everything you need to<br>
+                                    run you MCA business “right out of the box”.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="money_details">
+                        <p><img src="http://mcaprotools.s3-us-west-2.amazonaws.com/wp-content/uploads/2015/03/price47.png" alt="price47" width="885" height="151" class="aligncenter size-full wp-image-2242" srcset="http://mcaprotools.com/wp-content/uploads/2015/03/price47.png 885w, http://mcaprotools.com/wp-content/uploads/2015/03/price47-300x51.png 300w, http://mcaprotools.com/wp-content/uploads/2015/03/price47-100x17.png 100w, http://mcaprotools.com/wp-content/uploads/2015/03/price47-120x20.png 120w, http://mcaprotools.com/wp-content/uploads/2015/03/price47-500x85.png 500w" sizes="(max-width: 885px) 100vw, 885px"></p>
+                        <h2 style="margin-bottom: 25px;">(You only pay once for <span style="background-color: #ffe519; margin-bottom: 25px;">lifetime access to MCA PRO TOOLS!</span>)</h2>
+                        <div class="su-button-center"><a href="http://mcaprotools.com/checkout/?add-to-cart=171" class="su-button su-button-style-default btn-mca-buy-tools su-button-wide" style="color:#fff; width: 100%; background-color:#ff0000;border-color:#cc0000;border-radius:5px;-moz-border-radius:5px;-webkit-border-radius:5px" target="_self"><span style="color:#fff;padding:0px 70px;font-size:48px;line-height:96px;border-color:#ff4d4d;border-radius:5px;-moz-border-radius:5px;-webkit-border-radius:5px;text-shadow:none;-moz-text-shadow:none;-webkit-text-shadow:none"> <img src="http://mcaprotools.s3-us-west-2.amazonaws.com/wp-content/uploads/2015/03/left-arrow.png" alt="">BUY MCA PRO TOOLS<img src="http://mcaprotools.s3-us-west-2.amazonaws.com/wp-content/uploads/2015/03/mca-buy-arrow-right.png" alt=""></span></a></div>
+                    </div>
+                    <p><img src="http://mcaprotools.s3-us-west-2.amazonaws.com/wp-content/uploads/2015/03/feedback.png" alt="feedback" width="817" height="1516" class="aligncenter size-full wp-image-2041" srcset="http://mcaprotools.com/wp-content/uploads/2015/03/feedback.png 817w, http://mcaprotools.com/wp-content/uploads/2015/03/feedback-162x300.png 162w, http://mcaprotools.com/wp-content/uploads/2015/03/feedback-552x1024.png 552w, http://mcaprotools.com/wp-content/uploads/2015/03/feedback-54x100.png 54w, http://mcaprotools.com/wp-content/uploads/2015/03/feedback-120x223.png 120w, http://mcaprotools.com/wp-content/uploads/2015/03/feedback-500x928.png 500w" sizes="(max-width: 817px) 100vw, 817px">
+                    </p></div>
+                <p></p><center><p></p>
+                    <h1 style="font-size: 90px;">GET MCA PRO TOOLS NOW!</h1>
+                    <h2 style="margin-bottom: 45px; !important">Before We Close The Doors Or Raise The Price!</h2>
+                    <p></p></center><p></p>
+                <div class="fusion-fullwidth fullwidth-box how_much_head mca-pro-invite" style="background-attachment:scroll;background-image: url(http://mcaprotools.s3-us-west-2.amazonaws.com/wp-content/uploads/2015/03/how_much.jpg);background-position:center center;background-repeat:no-repeat;-webkit-background-size:cover;-moz-background-size:cover;-o-background-size:cover;background-size:cover;border-color:#eae9e9;border-bottom-width: 0px;border-top-width: 0px;border-bottom-style: solid;border-top-style: solid;padding-bottom:0px;padding-left:0px;padding-right:0px;padding-top:0px;"><div class="avada-row">
+                        <div class="su-button-center"><a href="http://mcaprotools.com/checkout/?add-to-cart=171" class="su-button su-button-style-default btn-mca-buy-tools su-button-wide" style="color:#fff; width: 100%; background-color:#ff0000;border-color:#cc0000;border-radius:5px;-moz-border-radius:5px;-webkit-border-radius:5px" target="_self"><span style="color:#fff;padding:0px 70px;font-size:48px;line-height:96px;border-color:#ff4d4d;border-radius:5px;-moz-border-radius:5px;-webkit-border-radius:5px;text-shadow:none;-moz-text-shadow:none;-webkit-text-shadow:none"> <img src="http://mcaprotools.s3-us-west-2.amazonaws.com/wp-content/uploads/2015/03/left-arrow.png" alt="">BUY MCA PRO TOOLS<img src="http://mcaprotools.s3-us-west-2.amazonaws.com/wp-content/uploads/2015/03/mca-buy-arrow-right.png" alt=""></span></a></div>
+                    </div></div>
+                <div class="container">
+                </div>
+            </div>
+        </div>
+    </div>
+
+<?php get_footer(); ?>
+
